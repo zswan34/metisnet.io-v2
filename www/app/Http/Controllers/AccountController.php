96f9f85f74980a1f55865424fc85802a25979b84
Zach Swan <zswan33@gmail.com>
2019-11-27 21:18:02 -0700

    updafadsf

--- www/app/Http/Controllers/AccountController.php
+++ www/app/Http/Controllers/AccountController.php
@@ -20,7 +20,7 @@ class AccountController extends Controller
 {
     public function index() {
         $locationData = GeoLocate::fetchClient();
-        return response()->json($locationData);
+        return response()->json($locationData->as);
         return view('index');
     }
 
